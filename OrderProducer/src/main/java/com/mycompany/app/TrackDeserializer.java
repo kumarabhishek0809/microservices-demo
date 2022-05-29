b161fe610d19b4b81e8983c05b60353f0d85f2d4
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-05-29 12:35:59 +0400

    commit kafka avro
    commit kafka avro

---
+++ OrderProducer/src/main/java/com/mycompany/app/TrackDeserializer.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.fernanda.maia.kafka.avro.Track;
+import lombok.SneakyThrows;
+public class TrackDeserializer implements Deserializer<Track> {
+    @Override
+    @SneakyThrows
+    public Track deserialize(String topic, byte[] data) {
+        Track response = null;
+        ObjectMapper mapper = new ObjectMapper();
+        response = mapper.readValue(data, Track.class);
+        return response;
+    }
+}
---
+++ OrderProducer/src/main/java/com/mycompany/app/TrackSerializer.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import org.apache.kafka.common.serialization.Serializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.fernanda.maia.kafka.avro.Track;
+import lombok.SneakyThrows;
+public class TrackSerializer implements Serializer<Track> {
+    @Override
+    @SneakyThrows
+    public byte[] serialize(String topic, Track track) {
+        ObjectMapper mapper = new ObjectMapper();
+        byte[] response = mapper.writeValueAsString(track).getBytes();
+        return response;
+    }
+}
