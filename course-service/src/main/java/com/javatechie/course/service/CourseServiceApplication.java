4b9738fbaa273ef43517eec3e2d5eb323bc45a1d
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-08-07 10:33:06 +0400

    updating Redis code
    updating Redis code

--- course-service/src/main/java/com/javatechie/course/service/CourseServiceApplication.java
+++ course-service/src/main/java/com/javatechie/course/service/CourseServiceApplication.java
@@ -1,14 +1,13 @@
 package com.javatechie.course.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @RestController
